ecf222bad8a12e300fcb9b76333448c5b1ddb83d
DESKTOP-20HJOKH\Hed <ugo@belfiore.fr>
2017-10-02 12:31:10 +0200

    Add graphic Engine class

---
+++ AsciiGame/AsciiGame/Header/GraphicEngine.h
@@ -0,0 +1,17 @@
+#pragma once			//que pour VS
+
+#include <iostream>
+
+
+class GraphicEngine
+{
+	//variable
+public:
+
+	//fonction
+public:
+	GraphicEngine();
+	~GraphicEngine();
+	void display();
+};
+
