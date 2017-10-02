484ccf2a98d5a3a646171e40ba96cc010a65882e
Jordan Bas <madjojo70@live.fr>
2017-10-02 13:46:52 +0200

    Add gameloop

--- AsciiGame/AsciiGame/AsciiGame.cpp
+++ AsciiGame/AsciiGame/AsciiGame.cpp
@@ -8,12 +8,38 @@
 using namespace std;
 
 int main()
-{
-	cout << "Hello Wolrd !" << endl;
-	
+{	
+	double lastTime = getCurrentTime();
+	bool keepRunning = true;
+	while (keepRunning)
+	{
+		double current = getCurrentTime();
+		double elapsed = current - lastTime;
+		keepRunning = processInput();
+		update(elapsed);
+		render();
+		lastTime = current;
+	}
 
-
-	system("pause");
     return (EXIT_SUCCESS);
 }
 
+double getCurrentTime()
+{
+	return 0.0;
+}
+
+bool processInput()
+{
+	return true;
+}
+
+void update(double elapsed)
+{
+
+}
+
+void render()
+{
+
+}
