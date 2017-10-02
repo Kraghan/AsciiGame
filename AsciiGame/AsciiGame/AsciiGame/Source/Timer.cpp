65217cc330a9a7632ed8bd6951bc91990c860e75
Jordan Bas <madjojo70@live.fr>
2017-10-02 16:11:04 +0200

    Add timer

--- AsciiGame/AsciiGame/AsciiGame/Source/Timer.cpp
+++ AsciiGame/AsciiGame/AsciiGame/Source/Timer.cpp
@@ -2,20 +2,38 @@
 
 /*explicit*/ Timer::Timer(void)
 {
-
+	QueryPerformanceCounter(&lastUpdateTime);
+	LARGE_INTEGER li_freq;
+	QueryPerformanceFrequency(&li_freq);
+	freq = li_freq.QuadPart;
+	freq /= 1000;
 }
 
 void Timer::start(void)
 {
-
+	QueryPerformanceCounter(&lastUpdateTime);
 }
 
-float Timer::getElapsedSeconds(bool restart = false)
+float Timer::getElapsedSeconds(bool restart)
 {
+	LARGE_INTEGER timeNow;
+	QueryPerformanceCounter(&timeNow);
+	LONGLONG elapsedLong = timeNow.QuadPart - lastUpdateTime.QuadPart;
 
+	float elapsed = (float)((float)elapsedLong / (float)freq);
+	elapsed /= 1000.0f;
+
+	if (restart)
+		lastUpdateTime = timeNow;
+
+	return elapsed;
 }
 
-unsigned long Timer::getElapsedMs(bool restart = false)
+double Timer::getElapsedMs(bool restart)
 {
+	LARGE_INTEGER timeNow;
+	QueryPerformanceCounter(&timeNow);
+	LONGLONG elapsedLong = timeNow.QuadPart - lastUpdateTime.QuadPart;
 
+	return (double)((float)elapsedLong / (float)freq);
 }
