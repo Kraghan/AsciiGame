c7a1e33c113a9f08cd9fd26af48f97c37fb0df08
DESKTOP-20HJOKH\Hed <ugo@belfiore.fr>
2017-10-02 11:35:01 +0200

    Add project Visuals + settings

---
+++ AsciiGame/AsciiGame/AsciiGame.cpp
@@ -0,0 +1,19 @@
+// AsciiGame.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello Wolrd !" << endl;
+	
+
+
+	system("pause");
+    return (EXIT_SUCCESS);
+}
+
